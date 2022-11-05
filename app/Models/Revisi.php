ef53d517b56215d9ff7d2973ab986bd13e5d97e0
matinrajab <matinrajab.mr@gmail.com>
2022-11-05 16:18:30 +0700

    second commit

--- app/Models/Revisi.php
+++ app/Models/Revisi.php
@@ -5,25 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use \APP\Models\Mahasiswa;
-use \APP\Models\Pembimbing;
 use \APP\Models\Ta;
+use \APP\Models\Pembimbing;
 
 class Revisi extends Model
 {
     use HasFactory;
-    public $with = ['mahasiswa', 'pembimbing', 'ta'];
+    public $with = ['mahasiswa', 'ta', 'pembimbing'];
 
     public function mahasiswa()
     {
         return $this->belongsTo(Mahasiswa::class, "id_mahasiswa");
     }
 
-    public function pembimbing()
-    {
-        return $this->belongsTo(Pembimbing::class, "id_pembimbing");
-    }
     public function ta()
     {
         return $this->belongsTo(Ta::class, "id_ta");
+    }
+
+    public function pembimbing()
+    {
+        return $this->belongsTo(Pembimbing::class, "id_pembimbing");
     }
 }
