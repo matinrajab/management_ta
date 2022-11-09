76971ccf7cc1b1e04f07696536fa30bc4f5cf5c8
Akbar0912 <ap411342@gmail.com>
2022-11-09 13:08:25 +0700

    update dashboard

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{asset('argon-dashboard-master')}}/assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="{{asset('argon-dashboard-master')}}/assets/img/favicon.png">
+  <title>
+    Manajement TA
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="{{asset('argon-dashboard-master')}}/assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="{{asset('argon-dashboard-master')}}/assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="{{asset('argon-dashboard-master')}}/assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="{{asset('argon-dashboard-master')}}/assets/css/argon-dashboard.css?v=2.0.4" rel="stylesheet" />
+</head>
+<body class="g-sidenav-show   bg-gray-100">
+  <div class="min-height-300 bg-primary position-absolute w-100"></div>
